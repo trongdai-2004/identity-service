c139baa1093f6bbfeafcb9d69286f265777aa463
Trong Dai <caotrongdai2004cd@gmail.com>
2026-08-30 15:19:52 +0700

    tạo request và service

---
+++ src/main/java/com/example/identity_service/service/UserService.java
@@ -0,0 +1,29 @@
+package com.example.identity_service.service;
+
+import com.example.identity_service.dto.request.UserCreationRequest;
+import com.example.identity_service.entity.User;
+import com.example.identity_service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+    public User createRequest(UserCreationRequest request) {
+        User user = new User();
+
+
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+
+        return userRepository.save(user);
+
+    }
+
+
+}
