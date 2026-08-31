92b5ae6243e82fff38e86d331929ff1e19c2dfdb
Trong Dai <caotrongdai2004cd@gmail.com>
2026-08-31 15:19:12 +0700

    quản lý validation

--- src/main/java/com/example/identity_service/dto/request/UserCreationRequest.java
+++ src/main/java/com/example/identity_service/dto/request/UserCreationRequest.java
@@ -1,9 +1,14 @@
 package com.example.identity_service.dto.request;
 
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 
 public class UserCreationRequest {
+    @Size(min = 3, message = "Username must be at least 3 characters")
     private String username;
+
+    @Size(min = 8, message = "password must be at least 8 character")
     private String password;
     private String firstName;
     private String lastName;
--- src/main/java/com/example/identity_service/exception/GlobalExceptionHanler.java
+++ src/main/java/com/example/identity_service/exception/GlobalExceptionHanler.java
@@ -1,6 +1,7 @@
 package com.example.identity_service.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -11,7 +12,10 @@ public class GlobalExceptionHanler {
     ResponseEntity<String> handlingRuntimeException(RuntimeException exception){
         return ResponseEntity.badRequest().body(exception.getMessage());
 
-
+    }
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    ResponseEntity<String>handlingValidation(MethodArgumentNotValidException exception){
+        return ResponseEntity.badRequest().body(exception.getFieldError().getDefaultMessage());
     }
 
 }
